8ab07dc9403a575b87d8a88a5e403b3d23669963
Jack Hou <jackhou1998@gmail.com>
2021-01-20 21:28:56 -0800

    Added modal functionality to project images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Nav Script
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav_link');
 
@@ -9,4 +10,19 @@ navLinks.forEach(link => {
     link.addEventListener('click', ()=> {
         document.body.classList.remove('nav-open');
     });
+});
+
+// Modal Script 
+var modal = document.querySelector(".modal");
+var modalImage = document.querySelector(".modal-content");
+
+const projectImg = document.querySelectorAll('.project-img');
+projectImg.forEach(item => {
+    item.addEventListener('click', event=>{
+        modal.style.display = "block";
+        modalImage.src = event.target.src;
+    });
+});
+document.querySelector(".close").addEventListener("click", () => {
+    modal.style.display = "none";
 });
